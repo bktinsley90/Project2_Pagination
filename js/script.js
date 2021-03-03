2eb5cae1bf8048b24c973ade37f3f1e8513a84c3
Brittany Tinsley <bktinsley90@gmail.com>
2021-03-03 17:30:36 -0500

    working on the showpage function

---
+++ js/script.js
@@ -0,0 +1,63 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+   
+// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+
+/*** 
+   Add your global variables that store the DOM elements you will 
+   need to reference and/or manipulate. 
+   
+   But be mindful of which variables should be global and which 
+   should be locally scoped to one of the two main functions you're 
+   going to create. A good general rule of thumb is if the variable 
+   will only be used inside of a function, then it can be locally 
+   scoped to that function.
+***/
+let list = document.querySelectorAll(".student-item") 
+//console.log(list)
+let studentsPerPage = list.length /10;
+//console.log(studentsPerPage)
+
+/*** 
+   Create the `showPage` function to hide all of the items in the 
+   list except for the ten you want to show.
+
+   Pro Tips: 
+     - Keep in mind that with a list of 54 students, the last page 
+       will only display four.
+     - Remember that the first student has an index of 0.
+     - Remember that a function `parameter` goes in the parens when 
+       you initially define the function, and it acts as a variable 
+       or a placeholder to represent the actual function `argument` 
+       that will be passed into the parens later when you call or 
+       "invoke" the function 
+***/
+const showPage = (list, page) => {
+   /*Loop over items in the list paramete
+      if the index of a list item is >= the index of the first item that should be shown on the page
+      && the list item index is <= the index of the last item that should be shown on the page, show it*/
+      
+      for(let i = 0; i < list.length; i++){
+         if((list[i] >= something) && (list[i] <= something)){
+        //show it
+         }
+          
+      }
+}
+
+
+
+
+/*** 
+   Create the `appendPageLinks function` to generate, append, and add 
+   functionality to the pagination buttons.
+***/
+
+
+
+
+
+// Remember to delete the comments that came with this file, and replace them with your own code comments.
